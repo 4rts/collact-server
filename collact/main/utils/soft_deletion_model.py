d9d04d59e44d13c65c7cb8a8c34fe9fbfb19925c
Jooyon Kim <gina611.kaist@gmail.com>
2019-11-08 19:22:21 +0900

    Fix wrong file for soft deletion model

--- collact/main/utils/soft_deletion_model.py
+++ collact/main/utils/soft_deletion_model.py
@@ -1,22 +1,50 @@
-import random
-import string
+from django.db import models
+from django.utils import timezone
 
 
-def random_string(length=16, punctutaions=False):
-    letters = string.ascii_letters + string.digits
-    if punctutaions:
-        letters += '!@#$^&'
-    return ''.join(random.SystemRandom().choice(letters) for _ in range(length))
+class SoftDeletionQuerySet(models.QuerySet):
+    def delete(self):
+        now = timezone.now()
+        updated_rows = super().update(deleted_dt=now, updated_dt=now)
+        return bool(updated_rows), updated_rows
+
+    def hard_delete(self):
+        return super().delete()
 
 
-def random_image(filename='test.png'):
-    from PIL import Image
-    size = (200,200)
-    color = (255,0,0,0)
-    img = Image.new("RGBA",size,color)
-    from io import BytesIO
-    f = BytesIO()
-    img.save(f, 'png')
-    f.name = 'test.png'
-    f.seek(0)
-    return f
+class SoftDeletionManager(models.Manager):
+    """
+    RedisCacheManager 등 여러 manager들과 함께 상속될 땐
+    항상 맨 마지막에 들어가야 super()가 models.Manager를 가리키게 됨
+    """
+    queryset_class = SoftDeletionQuerySet
+
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop('alive_only', True)
+        super().__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.alive_only:
+            return self.queryset_class(self.model).filter(deleted_dt=None)
+        return self.queryset_class(self.model)
+
+
+class SoftDeletionModel(models.Model):
+    """
+    RedisCacheModel 등 여러 model들과 함께 상속될 땐
+    항상 맨 마지막에 들어가야 super()가 models.Model을 가리키게 됨
+    """
+    deleted_dt = models.DateTimeField(blank=True, null=True, db_index=True)
+
+    objects = SoftDeletionManager()
+    all_objects = SoftDeletionManager(alive_only=False)
+
+    def delete(self):
+        self.deleted_dt = timezone.now()
+        super().save()
+
+    def hard_delete(self):
+        super().delete()
+
+    class Meta:
+        abstract = True
