003d1612ff267c8be4418637f417f7d5eab46d53
Jooyon Kim <gina611.kaist@gmail.com>
2019-11-22 14:26:15 +0900

    Add simple serilizers

--- collact/api/serializers.py
+++ collact/api/serializers.py
@@ -4,6 +4,31 @@ from api.models import Artist, Art, Collabo, FavoriteArtist, FavoriteCollabo, Co
 from main.utils.serializers import PrepareModelSerializer
 
 
+class ArtistSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ('id', 'user', 'profile', 'name')
+
+
+class CollaboSimpleSerializer(serializers.ModelSerializer):
+    main_artist = serializers.SerializerMethodField()
+    sub_artist = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Collabo
+        fields = ('id', 'file', 'main_artist', 'sub_artist')
+
+    @staticmethod
+    def get_main_artist(collabo):
+        return ArtistSimpleSerializer(collabo.main_artist).data
+
+    @staticmethod
+    def get_sub_artist(collabo):
+        return ArtistSimpleSerializer(collabo.sub_artist).data
+
+##################
+
+
 class ArtSerializer(PrepareModelSerializer):
     class Meta:
         model = Art
@@ -16,12 +41,6 @@ class CollaboApplicationSerializer(PrepareModelSerializer):
         model = CollaboApplication
         exclude = ()
         read_only_fields = ('id',)
-
-
-class ArtistSimpleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Artist
-        fields = ('id', 'user', 'profile', 'name')
 
 
 class CollaboSerializer(PrepareModelSerializer):
@@ -51,15 +70,45 @@ class FavoriteCollaboSerializer(PrepareModelSerializer):
 
 
 class ArtistSerializer(PrepareModelSerializer):
+    favorite_artists = serializers.SerializerMethodField()
+    favorite_collabos = serializers.SerializerMethodField()
+
+    waiting_collabos = serializers.SerializerMethodField()
+    working_collabos = serializers.SerializerMethodField()
+    completed_collabos = serializers.SerializerMethodField()
+
+    followers = serializers.SerializerMethodField()
+
     class Meta:
         model = Artist
         fields = ('id', 'user', 'description', 'profile', 'collaboration_types', 'creation_type',
-                  'arts', 'favorite_artists', 'favorite_collabos',
-                  'name', 'waiting_collabos', 'working_collabos', 'completed_collabos', 'followers')
+                  'arts', 'name', 'favorite_artists', 'favorite_collabos',
+                  'waiting_collabos', 'working_collabos', 'completed_collabos', 'followers')
         read_only_fields = ('id',)
         expandable_fields = {
-            'favorite_collabos': {'serializer': CollaboSerializer, 'many': True},
-            'waiting_collabos': {'serializer': CollaboSerializer, 'many': True},
-            'working_collabos': {'serializer': CollaboSerializer, 'many': True},
-            'completed_collabos': {'serializer': CollaboSerializer, 'many': True},
+            'arts': {'serializer': ArtSerializer, 'many': True},
         }
+
+    @staticmethod
+    def get_favorite_artists(artist):
+        return ArtistSimpleSerializer(artist.favorite_artists, many=True).data
+
+    @staticmethod
+    def get_favorite_collabos(artist):
+        return CollaboSimpleSerializer(artist.favorite_collabos, many=True).data
+
+    @staticmethod
+    def get_waiting_collabos(artist):
+        return CollaboSimpleSerializer(artist.waiting_collabos, many=True).data
+
+    @staticmethod
+    def get_working_collabos(artist):
+        return CollaboSimpleSerializer(artist.working_collabos, many=True).data
+
+    @staticmethod
+    def get_completed_collabos(artist):
+        return CollaboSimpleSerializer(artist.completed_collabos, many=True).data
+
+    @staticmethod
+    def get_followers(artist):
+        return ArtistSimpleSerializer(artist.followers, many=True).data
