deb3199bf776be92ddb9a827ad752949b2fbb51d
Jooyon Kim <gina611.kaist@gmail.com>
2019-11-22 11:10:07 +0900

    Add collabo application viewset in urls

--- collact/api/urls.py
+++ collact/api/urls.py
@@ -2,12 +2,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import ArtistViewSet, ArtViewSet, CollaboViewSet, PingView, FavoriteArtistViewSet, \
-    FavoriteCollaboViewSet, CollaboRegisterView
+    FavoriteCollaboViewSet, CollaboRegisterView, CollaboApplicationViewSet
 
 router = DefaultRouter()
 router.register('artists', ArtistViewSet, basename='artists')
 router.register('arts', ArtViewSet, basename='arts')
 router.register('collabos', CollaboViewSet, basename='collabos')
+router.register('collabo_applications', CollaboApplicationViewSet, basename='collabo_applications')
 router.register('favorite_artists', FavoriteArtistViewSet, basename='favorite_artists')
 router.register('favorite_collabos', FavoriteCollaboViewSet, basename='favorite_collabos')
 
